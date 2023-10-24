e052f4ad74b3eca6eadb5f92da010d755f28ac6d
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2023-10-24 12:47:25 -0400

    Add myFlix API

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,38 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { MovieCard } from "../MovieCard/movie-card";
 
 import { MovieView } from "../MovieView/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Zootopia",
-      image: "https://lumiere-a.akamaihd.net/v1/images/movie_poster_zootopia_866a1bf2.jpeg",
-      description: "In a city of anthropomorphic animals, a rookie bunny cop and a cynical con artist fox must work together to uncover a conspiracy.",
-      director: "Byron Howard, Rich Moore, Jared Bush",
-      genre: "Comedy"
-    },
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://huff-movies-aa259f3af035.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image:`https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+            description: doc.description,
+            director: doc.director_name?.[0],
+            genre: doc.genre_name?.[0]
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
     
-    {
-      id: 2,
-      title: "The Longest Yard",
-      image: "https://m.media-amazon.com/images/I/6170kKD3zGL._AC_UF894,1000_QL80_.jpg",
-      description: "Prison inmates form a football team to challenge the prison guards.",
-      director: "Peter Segal",
-      genre: "Comedy"
-    },
-    {
-      id: 3,
-      title: "The Hobbit: An Unexpected Journey",
-      image: "https://www.themoviedb.org/t/p/w500/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg",
-      description: "An Unexpected Journey tells the tale of Bilbo Baggins (Martin Freeman), who is convinced by the wizard Gandalf (Ian McKellen) to accompany thirteen Dwarves, led by Thorin Oakenshield (Richard Armitage), on a quest to reclaim the Lonely Mountain from the dragon Smaug.",
-      director: "Peter Jackson",
-      genre: "Action, Adventure"
-    }
- 
-  ]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
